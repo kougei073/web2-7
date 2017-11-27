3b6a84c8f5c795eeda9d127d3edc5107d59dc580
kougei073 <g1627073@st.t-kougei.ac.jp>
2017-11-27 17:33:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var canvas = doucument.getElementById('canvas');
 canvas.width = 500;
 canvas.height = 300;
 
-var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+var gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
 
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
