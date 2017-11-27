d559da430c2418863581a71c84c7ad6e4796086f
kougei073 <g1627073@st.t-kougei.ac.jp>
2017-11-27 18:54:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -75,9 +75,9 @@ onload = function(){
     var rad = (count % 360) * Math.PI / 180;
 
     // モデルを回転
-    m.identity(wMatrix);
-    m.translate(wMatrix, [0.0, 0.0, 0.0], wMatrix);//モデルの位置
-    m.rotate(wMatrix, rad, [1, 1, 1], wMatrix);//回転方向
+   // m.identity(wMatrix);
+    //m.translate(wMatrix, [0.0, 0.0, 0.0], wMatrix);//モデルの位置
+    //m.rotate(wMatrix, rad, [1, 1, 1], wMatrix);//回転方向
 
     // モデルの座標変換行列を完成させレンダリングする
     m.multiply(vpMatrix, wMatrix, wvpMatrix);
