3c326e27a77f22cbdc1b43c1ca1fd83427b91a4d
kougei073 <g1627073@st.t-kougei.ac.jp>
2017-11-27 17:29:16 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+onload = function(){
+var canvas = doucument.getElementById('canvas');
+canvas.width = 500;
+canvas.height = 300;
+
+var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+};
